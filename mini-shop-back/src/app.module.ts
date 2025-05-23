6582b6b49c8eefb009cc9c19dbb2c6587dd8d9f9
jegen <jean-ely.gendrau@laplateforme.io>
2025-05-23 17:02:58 +0200

    # Added @nestjs/config dependency and updated database configuration in app.module.ts

--- mini-shop-back/src/app.module.ts
+++ mini-shop-back/src/app.module.ts
@@ -3,15 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
+      port: 3320,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
       database: 'test',
       entities: [],
       synchronize: true,
